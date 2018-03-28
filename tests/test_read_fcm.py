d66321d20f3d14a56ac6909050fa29eee6d237b3
Paul Müller <dev@craban.de>
2018-03-28 21:15:11 +0200

    flake8: cleanup of tests

--- tests/test_read_fcm.py
+++ tests/test_read_fcm.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import numpy as np
 import os
-from os.path import join, dirname, abspath, split
-import pytest
-import sys
 import tempfile
 
-# Add parent directory to beginning of path variable
-DIR = dirname(abspath(__file__))
-sys.path.insert(0, split(DIR)[0])
+import numpy as np
+import pytest
 
 import fcswrite
 
@@ -21,22 +16,21 @@ def test_read_fcm():
     from mock import patch, MagicMock
     module_mock = MagicMock()
 
-    with patch.dict('sys.modules', **{ 
-            'matplotlib.nxutils': module_mock,
-            'matplotlib.nxutils.points_inside_poly': module_mock,
-        }):
+    with patch.dict('sys.modules', **{
+        'matplotlib.nxutils': module_mock,
+        'matplotlib.nxutils.points_inside_poly': module_mock,
+    }):
         from fcm import loadFCS
 
-        fname=tempfile.mktemp(suffix=".fcs", prefix="fcm_read_test")
-        data = 1.0*np.arange(400).reshape((100,4))
-        chn_names= ['c1', 'channel_2', 'Channel 3', '4jjjjjjjjj']
+        fname = tempfile.mktemp(suffix=".fcs", prefix="fcm_read_test")
+        data = 1.0*np.arange(400).reshape((100, 4))
+        chn_names = ['c1', 'channel_2', 'Channel 3', '4jjjjjjjjj']
         fcswrite.write_fcs(filename=fname,
                            chn_names=chn_names,
                            data=data,
                            compat_chn_names=False
                            )
 
-        
         ldata = loadFCS(fname)
         assert ldata.shape == data.shape
         assert ldata.channels == chn_names
@@ -51,4 +45,3 @@ if __name__ == "__main__":
     for key in list(loc.keys()):
         if key.startswith("test_") and hasattr(loc[key], "__call__"):
             loc[key]()
-    
--- tests/test_write_simple.py
+++ tests/test_write_simple.py
@@ -1,25 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import hashlib
-import numpy as np
 import os
-from os.path import join, dirname, abspath, split
-import pytest
-import sys
 import tempfile
 
-# Add parent directory to beginning of path variable
-DIR = dirname(abspath(__file__))
-sys.path.insert(0, split(DIR)[0])
+import numpy as np
 
 import fcswrite
 
 
 def test_write_fcs():
     """test that fcm can read the data files"""
-    fname=tempfile.mktemp(suffix=".fcs", prefix="write_test")
-    data = 1.0*np.arange(400).reshape((100,4))
-    chn_names= ['cha', 'chb', 'ch3', 'ch4']
+    fname = tempfile.mktemp(suffix=".fcs", prefix="write_test")
+    data = 1.0*np.arange(400).reshape((100, 4))
+    chn_names = ['cha', 'chb', 'ch3', 'ch4']
     fcswrite.write_fcs(filename=fname,
                        chn_names=chn_names,
                        data=data
@@ -33,7 +27,7 @@ def test_write_fcs():
     data = data.tostring()
     hasher = hashlib.md5()
     hasher.update(data)
-    hexval=hasher.hexdigest()
+    hexval = hasher.hexdigest()
     assert hexval == "bbb090b99ccb1c9b1d8b43b83eddc7d3"
     os.remove(fname)
 
@@ -44,4 +38,3 @@ if __name__ == "__main__":
     for key in list(loc.keys()):
         if key.startswith("test_") and hasattr(loc[key], "__call__"):
             loc[key]()
-    
